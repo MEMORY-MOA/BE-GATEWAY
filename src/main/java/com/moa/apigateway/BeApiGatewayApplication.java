bd87b79ebd38826cd1fdbf82ba43d0e5b0029ea3
ahinkim <ahin98@naver.com>
2023-09-17 12:11:11 +0900

    feat: gateway 구현

---
+++ src/main/java/com/moa/apigateway/BeApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.moa.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/moa/apigateway/filter/AuthorizationHeaderFilter.java
@@ -0,0 +1,83 @@
+package com.moa.apigateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import io.jsonwebtoken.Jwts;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.config> {
+	Environment env;
+
+	public AuthorizationHeaderFilter (Environment env) {
+		super(config.class);
+		this.env = env;
+	}
+
+	public static class config {
+
+	}
+	@Override
+	public GatewayFilter apply(config config) {
+		return ((exchange, chain) -> {
+			ServerHttpRequest request = exchange.getRequest();
+
+			if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+				return onError(exchange, "authorization header가 없습니다.", HttpStatus.UNAUTHORIZED);
+			}
+
+			String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+			String jwt = authorizationHeader.replace("Bearer", "");
+
+			if (!isJwtValid(jwt)) {
+				return onError(exchange, "JWT token이 유효하지 않습니다.", HttpStatus.UNAUTHORIZED);
+			}else {
+				String subject = extractMemberIdFromJwt(jwt);
+				request = exchange.getRequest().mutate()
+					.header("member", subject)
+					.build();
+			}
+			return chain.filter(exchange.mutate().request(request).build());
+		});
+	}
+
+	private boolean isJwtValid(String jwt) {
+		boolean returnValue = true;
+		String subject = null;
+		try {
+			subject = extractMemberIdFromJwt(jwt);
+		} catch (Exception ex) {
+			returnValue = false;
+		}
+
+		if (subject == null || subject.isEmpty()) {
+			returnValue = false;
+		}
+
+		return returnValue;
+	}
+
+	private String extractMemberIdFromJwt(String jwt) {
+		String subject = Jwts.parser().setSigningKey(env.getProperty("token.secret"))
+			.parseClaimsJws(jwt).getBody()
+			.getSubject();
+		return subject;
+	}
+	private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+		ServerHttpResponse response = exchange.getResponse();
+		response.setStatusCode(httpStatus);
+
+		log.error(err);
+		return response.setComplete();
+	}
+}
